20b170e98967096a5f60a54ddfdb84c3a5baf588
voomilo <kostykevichs13@gmail.com>
2025-09-29 22:40:21 +0300

    генератор рандомных чисел final

--- RandomGenerator.java
+++ RandomGenerator.java
@@ -6,20 +6,20 @@ public class RandomGenerator {
         Random random = new Random();
         int[] numbers = new int[5];
 
-        System.out.println("=== Генератор случайных чисел ===");
+        System.out.println("Р“РµРЅРµСЂР°С‚РѕСЂ СЃР»СѓС‡Р°Р№РЅС‹С… С‡РёСЃРµР»");
 
-        // Генерация чисел
-        System.out.println("5 случайных чисел от 1 до 100:");
+        // Р“РµРЅРµСЂР°С†РёСЏ С‡РёСЃРµР»
+        System.out.println("5 СЃР»СѓС‡Р°Р№РЅС‹С… С‡РёСЃРµР» РѕС‚ 1 РґРѕ 100:");
         for (int i = 0; i < 5; i++) {
             numbers[i] = random.nextInt(100) + 1;
-            System.out.println("Число " + (i + 1) + ": " + numbers[i]);
+            System.out.println("Р§РёСЃР»Рѕ " + (i + 1) + ": " + numbers[i]);
         }
 
-        // Сортировка и вывод
-        System.out.println("\nОтсортированные числа:");
+        // РЎРѕСЂС‚РёСЂРѕРІРєР° Рё РІС‹РІРѕРґ
+        System.out.println("\nРћС‚СЃРѕСЂС‚РёСЂРѕРІР°РЅРЅС‹Рµ С‡РёСЃР»Р°:");
         Arrays.sort(numbers);
         for (int i = 0; i < numbers.length; i++) {
-            System.out.println("Число " + (i + 1) + ": " + numbers[i]);
+            System.out.println("Р§РёСЃР»Рѕ " + (i + 1) + ": " + numbers[i]);
         }
     }
 }
