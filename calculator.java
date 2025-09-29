3cd4390bd9558e425dfdfd9ff46bdbfc7c22d29b
voomilo <kostykevichs13@gmail.com>
2025-09-29 20:47:30 +0300

    добавил сложение

--- calculator.java
+++ calculator.java
@@ -1,5 +1,13 @@
 public class calculator {
+    public static double add(double a, double b) {
+        return a + b;
+    }
+
     public static void main(String[] args) {
+        double num1 = 10.5;
+        double num2 = 5.2;
+
         System.out.println("Калькулятор");
+        System.out.println(num1 + " + " + num2 + " = " + add(num1, num2));
     }
 }
