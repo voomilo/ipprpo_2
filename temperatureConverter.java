9d88ccb3cc7c53ba0da5c2670edfdeaaaeddc849
voomilo <kostykevichs13@gmail.com>
2025-09-29 22:22:54 +0300

    добавил перевод из цельсия в фаренгейт

--- temperatureConverter.java
+++ temperatureConverter.java
@@ -1,5 +1,12 @@
 public class temperatureConverter {
+    public static double celsiusToFahrenheit(double celsius) {
+        return (celsius * 9/5) + 32;
+    }
+
     public static void main(String[] args) {
+        double celsius = 25.0;
+
         System.out.println("Конвертер температур");
+        System.out.println(celsius + "°C = " + celsiusToFahrenheit(celsius) + "°F");
     }
 }
