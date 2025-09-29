c7f491d82bb2ee4319c80d8a5de12dcf1fbc8661
voomilo <kostykevichs13@gmail.com>
2025-09-29 22:35:41 +0300

    добавил генерацию чисел

--- RandomGenerator.java
+++ RandomGenerator.java
@@ -2,6 +2,14 @@ import java.util.Random;
 
 public class RandomGenerator {
     public static void main(String[] args) {
+        Random random = new Random();
+
         System.out.println("Генератор случайных чисел");
+        System.out.println("5 случайных чисел от 1 до 100:");
+
+        for (int i = 1; i <= 5; i++) {
+            int randomNumber = random.nextInt(100) + 1;
+            System.out.println("Число " + i + ": " + randomNumber);
+        }
     }
 }
