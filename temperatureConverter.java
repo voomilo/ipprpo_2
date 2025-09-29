143f825d9174b2e6122ee7e50a642d71419b18cf
voomilo <kostykevichs13@gmail.com>
2025-09-29 22:24:43 +0300

    конвертер final

--- temperatureConverter.java
+++ temperatureConverter.java
@@ -3,10 +3,20 @@ public class temperatureConverter {
         return (celsius * 9/5) + 32;
     }
 
+    public static double fahrenheitToCelsius(double fahrenheit) {
+        return (fahrenheit - 32) * 5/9;
+    }
+
     public static void main(String[] args) {
         double celsius = 25.0;
+        double fahrenheit = 77.0;
 
         System.out.println("Конвертер температур");
         System.out.println(celsius + "°C = " + celsiusToFahrenheit(celsius) + "°F");
+        System.out.println(fahrenheit + "°F = " + fahrenheitToCelsius(fahrenheit) + "°C");
+
+        System.out.println("0°C = " + celsiusToFahrenheit(0) + "°F");
+        System.out.println("100°C = " + celsiusToFahrenheit(100) + "°F");
+        System.out.println("32°F = " + fahrenheitToCelsius(32) + "°C");
     }
 }
