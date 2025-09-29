46a8dace814d66f6563254393e1b79abf094440f
voomilo <kostykevichs13@gmail.com>
2025-09-29 22:36:32 +0300

    добавил сортировку

--- RandomGenerator.java
+++ RandomGenerator.java
@@ -1,15 +1,25 @@
 import java.util.Random;
+import java.util.Arrays;
 
 public class RandomGenerator {
     public static void main(String[] args) {
         Random random = new Random();
+        int[] numbers = new int[5];
 
-        System.out.println("Генератор случайных чисел");
+        System.out.println("=== Генератор случайных чисел ===");
+
+        // Генерация чисел
         System.out.println("5 случайных чисел от 1 до 100:");
+        for (int i = 0; i < 5; i++) {
+            numbers[i] = random.nextInt(100) + 1;
+            System.out.println("Число " + (i + 1) + ": " + numbers[i]);
+        }
 
-        for (int i = 1; i <= 5; i++) {
-            int randomNumber = random.nextInt(100) + 1;
-            System.out.println("Число " + i + ": " + randomNumber);
+        // Сортировка и вывод
+        System.out.println("\nОтсортированные числа:");
+        Arrays.sort(numbers);
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println("Число " + (i + 1) + ": " + numbers[i]);
         }
     }
 }
